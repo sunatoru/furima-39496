ba8ccf7acfabd2dfc528b21ef7234ea9221cc610
tano <supright34@gmail.com>
2023-09-22 00:57:52 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+          message: "は漢字か平仮名かかなを含める必要があります" }
+      
+        validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+          message: "は漢字と平仮名のみを含める必要があります" }
+
+          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
+            message: "はカナ文字のみを含める必要があります" }
+        
+          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
+            message: "はカナ文字のみを含める必要があります" }
+
+            validates :birth_date, presence: true
+
+            validate :password_complexity
+  def password_complexity
+    if password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*\d).+/)
+      errors.add :password, "は少なくとも1つの英字と1つの数字を含む必要があります"
+    end
+  end
 end
