d8dde15bb338f0c553bc5243935b623beecc75db
tano <supright34@gmail.com>
2023-09-23 17:17:31 +0900

    rubocopを使ってコードを綺麗に記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
-          message: "は漢字か平仮名かかなを含める必要があります" }
-      
-        validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
-          message: "は漢字と平仮名のみを含める必要があります" }
+  validates :nickname, presence: true
 
-          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-            message: "はカナ文字のみを含める必要があります" }
-        
-          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-            message: "はカナ文字のみを含める必要があります" }
+  validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+                                                   message: 'は漢字か平仮名かかなを含める必要があります' }
 
-            validates :birth_date, presence: true
+  validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+                                                  message: 'は漢字と平仮名のみを含める必要があります' }
 
-            validate :password_complexity
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
+                                                        message: 'はカナ文字のみを含める必要があります' }
+
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
+                                                       message: 'はカナ文字のみを含める必要があります' }
+
+  validates :birth_date, presence: true
+
+  validate :password_complexity
   def password_complexity
-    if password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*\d).+/)
-      errors.add :password, "は少なくとも1つの英字と1つの数字を含む必要があります"
-    end
+    return unless password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*\d).+/)
+
+    errors.add :password, 'は少なくとも1つの英字と1つの数字を含む必要があります'
   end
 end
