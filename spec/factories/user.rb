16fc443437644e2f38cec17c499ef83da3a15894
tano <supright34@gmail.com>
2023-09-23 21:44:33 +0900

    FactoryBotで生成されるuserのpasswordをランダムに設定

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 4) }
     email                 { Faker::Internet.email }
-    password              { '1a12ra' }
+    password              { "1a#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     first_name            { '大輔' }
     last_name             { '高橋' }
