a283de8d68055e4314562154dac775ea0d86288f
tano <supright34@gmail.com>
2023-09-23 21:34:28 +0900

    レビューをいただいて修正すべき点を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                                                    message: 'は漢字か平仮名かかなを含める必要があります' }
 
-  validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/,
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                                                   message: 'は漢字と平仮名のみを含める必要があります' }
 
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
@@ -22,7 +22,7 @@ class User < ApplicationRecord
 
   validate :password_complexity
   def password_complexity
-    return unless password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*\d).+/)
+    return unless password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/)
 
     errors.add :password, 'は少なくとも1つの英字と1つの数字を含む必要があります'
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,20 +38,40 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include "First name can't be blank"
       end
+      it 'first_nameに半角文字があると登録できない' do
+        @user.first_name = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'First name は漢字か平仮名かかなを含める必要があります'
+      end
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "Last name can't be blank"
+      end
+      it 'last_nameに半角文字があると登録できない' do
+        @user.last_name = 'sample'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Last name は漢字と平仮名のみを含める必要があります'
       end
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "First name kana can't be blank"
       end
+      it 'first_name_kanaにカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれている登録できない' do
+        @user.first_name_kana = 'かな文字1!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'First name kana はカナ文字のみを含める必要があります'
+      end
       it 'last_name_kanaが空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "Last name kana can't be blank"
+      end
+      it 'last_name_kanaにカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれているできない' do
+        @user.last_name_kana = 'teも寺1!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Last name kana はカナ文字のみを含める必要があります'
       end
       it 'birth_dateが空では登録できない' do
         @user.birth_date = ''
@@ -72,6 +92,12 @@ RSpec.describe User, type: :model do
       end
       it 'passwordが数字のみの場合では登録できない' do
         @user.password = '313222222'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password は少なくとも1つの英字と1つの数字を含む必要があります'
+      end
+      it 'passwordに全角文字が入っている場合は登録できない' do
+        @user.password = 'password全角文字'
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include 'Password は少なくとも1つの英字と1つの数字を含む必要があります'
