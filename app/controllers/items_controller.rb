5455d8ee3c6141f91960ca1b3a327529964e5087
tano <supright34@gmail.com>
2023-09-25 23:32:07 +0900

    画像アップロード機能の実装(indexの見た目もそれに伴い実装)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    redirect_to '/'
+  end
+
+  private
+  
+  def items_params
+    params.require(:item).permit(:item_name, :item_content, :category_id, :condition_id, :delivery_country_id, :delivery_date_id, :delivery_charge_id, :item_price, :image).marge(user_id: current_user.id)
   end
 end
