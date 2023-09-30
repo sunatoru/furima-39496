9c2bb5fc1c097f77d63b6798dcb1e38474098897
tano <supright34@gmail.com>
2023-09-30 16:47:53 +0900

    商品出品ページまでの遷移と出品ページとnew,createアクションを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
-    redirect_to '/'
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
-  
-  def items_params
-    params.require(:item).permit(:item_name, :item_content, :category_id, :condition_id, :delivery_country_id, :delivery_date_id, :delivery_charge_id, :item_price, :image).marge(user_id: current_user.id)
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_content, :category_id, :condition_id, :delivery_country_id, :delivery_date_id,
+                                 :delivery_charge_id, :item_price, :image).merge(user_id: current_user.id)
   end
 end
