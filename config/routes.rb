e47a183427b5aec3c7a4284a836bd1c21753a0eb
tano <supright34@gmail.com>
2023-10-01 20:59:34 +0900

    indexアクションへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:new, :create,:index]
 end
