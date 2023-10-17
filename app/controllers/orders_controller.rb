a6165a9f26d5606383bfb516f01bc71d9266d65f
tano <supright34@gmail.com>
2023-10-17 23:35:04 +0900

    決済処理を記述(コントローラーとビューにも)とitemの方のjsも修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,50 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_item, only: [:index, :create]
+  before_action :already_purchase, only: [:index, :create] # 購入者及びログインユーザーと未登録ユーザー
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
 
   private
+
   def find_item
     @item = Item.find(params[:item_id])
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :city, :address, :building, :telephone_number, :delivery_country_id).merge(item_id: params[:item_id], user_id: current_user.id )
+    params.require(:order_address).permit(
+      :postal_code, :city, :address, :building, :telephone_number, :delivery_country_id
+    ).merge(
+      item_id: params[:item_id], user_id: current_user.id, token: params[:token]
+    )
+  end
+
+  def already_purchase
+    return unless user_signed_in? && (@item.order.present? || current_user == @item.user)
+
+    redirect_to root_path
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.item_price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+ function item_price(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -6,6 +6,9 @@ window.addEventListener('load', function(){
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1))
-  })
-})
+  });
+}
 
+
+window.addEventListener('load', item_price)
+window.addEventListener("turbo:render", item_price);
