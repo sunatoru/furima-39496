764f910cbf6d80a23a10db64c2e7d2cc64ff2e94
tano <supright34@gmail.com>
2023-10-17 23:35:49 +0900

    決済処理と購入機能をテストで記述

--- spec/factories/order_adress.rb
+++ spec/factories/order_adress.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :order_address do
-    postal_code { Faker::Number.number(digits: 3).to_s + '-' + Faker::Number.number(digits: 4).to_s }
+    postal_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     delivery_country_id { Faker::Number.between(from: 2, to: 48) }
     city { '東京都千代田区' }
     address { Faker::Address.street_address }
     building { Faker::Address.secondary_address }
-    telephone_number { Faker::Number.number(digits: rand(3) + 10) }
+    telephone_number { Faker::Number.number(digits: rand(10..11)) }
     user_id { Faker::Number.non_zero_digit }
     item_id { Faker::Number.non_zero_digit }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
